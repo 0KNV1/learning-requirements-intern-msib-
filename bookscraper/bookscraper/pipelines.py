42d7aec5ccdf80684fcb4226219b4302866c7add
= <=>
2023-08-16 08:42:55 +0700

    first commit

---
+++ bookscraper/bookscraper/pipelines.py
@@ -0,0 +1,45 @@
+import pymysql
+
+class MySQLDemoPipeline:
+
+    def __init__(self):
+        ## Connection Details
+        hostname = 'localhost'
+        username = 'root'
+        password = 'root' # your password
+        database = 'book-test1'
+
+        ## Create/Connect to database
+        self.connection = pymysql.connect(host=hostname, user=username, password=password, db=database, cursorclass=pymysql.cursors.DictCursor)
+        
+        ## Create cursor, used to execute commands
+        self.cur = self.connection.cursor()
+        
+        ## Create quotes table if none exists
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS quotes(
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+            title VARCHAR(255),
+            category VARCHAR(255),
+            description VARCHAR(255)
+        )
+        """)
+
+    def process_item(self, item, spider):
+
+        ## Define insert statement
+        self.cur.execute(""" insert into quotes (title, category, description) values (%s,%s,%s)""", (
+            item["title"],
+            str(item["category"]),
+            item["description"]
+        ))
+
+        ## Execute insert of data into database
+        self.connection.commit()
+        return item
+
+    def close_spider(self, spider):
+
+        ## Close cursor & connection to database 
+        self.cur.close()
+        self.connection.close()
---
+++ bookscraper/bookscraper/readme.md
@@ -0,0 +1,44 @@
+
+
+Installing Scrapy
+## Following tutorial"https://www.youtube.com/watch?v=mBoX_JCKZTE"
+Scrapy is a Python-based web scraping framework. This README provides instructions for installing Scrapy on your local machine.
+
+Prerequisites
+
+Before installing Scrapy, make sure you have the following software installed:
+
+    Python (version 3.6 or higher)
+    pip (Python package manager)
+    Microsoft Visual C++ 14.0 (required for some Windows installations)
+
+
+Installing Scrapy
+
+To install Scrapy, follow these steps:
+
+    Open a terminal window.
+    Run the following command to install Scrapy using pip:
+
+
+pip install scrapy
+
+
+    If the installation is successful, you should see a message similar to the following:
+
+
+Successfully installed Scrapy-2.5.1 ...
+
+
+    To verify that Scrapy is installed correctly, run the following command:
+
+
+scrapy version
+
+
+This should output the version number of Scrapy.
+![Alt text](image.png)
+dont forget install requirement
+![Alt text](image-1.png)
+and connect to database
+![Alt text](image-2.png)
